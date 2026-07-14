e5b8cde493a73ae063f9825ca343194930e7a3ca
Nataraj305 <natarajmohan849@gmail.com>
2026-07-14 11:28:17 +0530

    Complete Week 3 Spring Core exercises

--- Week 3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,13 +1,60 @@
 package com.cognizant.spring_learn;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cognizant.spring_learn.model.Country;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-	}
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
 
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayDate();
+        displayCountry();
+    }
+
+    public static void displayDate() throws Exception {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+        Date date = format.parse("31/12/2018");
+
+        LOGGER.debug("Date : {}", date);
+
+        LOGGER.info("END");
+    }
+
+    public static void displayCountry() {
+
+        LOGGER.info("Country Bean START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country);
+
+        LOGGER.info("Country Bean END");
+    }
 }
